d8cf8decd1c74d394daf1e1ec44a68298b49be4b
AlejandroAlonsoFigueroa23 <alexalfi00@gmail.com>
2025-01-16 19:08:35 -0600

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,1 +1,25 @@
 
+/*
+  Estas configuraciones son especificas para este repositorio/proyecto
+*/
+// Configuraciones generales
+project_app = "itam-reports"
+jdk_tool = 'Java-OP11'
+
+// Librerias que se usaran
+libraries{
+    maven    
+}
+
+// Variables a usar por ambiente
+application_environments{
+    sandbox{
+        stage = 'sandbox'
+    }
+    prod{
+        ignore = true
+        stage = 'production'
+    }
+}
+
+release_active = true
