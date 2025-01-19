e4b96b208e22ebf643eadbb96b14e631a9a52c7d
AlejandroAlonsoFigueroa23 <alexalfi00@gmail.com>
2025-01-18 22:23:21 -0600

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,17 +1,31 @@
-// Librerias que se usaran
-libraries{
-    maven    
+jdk_tool = ''
+maven_install = 'openpay-maven-3.9.6'
+
+libraries {
+	maven
+	ecr
 }
 
-application_environments{
-    sandbox{
-        stage = 'sandbox'
+application_environments {
+    dev {
+    	ignore = false
+    	ecr_registry = '378653672129.ecr_no_existe.amazonaws.com'
+        ecr_region = 'us-east-1'
+        aws_cred_id = 'aws-openpay-dev'
+        ecr_repo_name = 'ecr_no_existe_jejej'
     }
-    prod{
-        ignore = true
-        stage = 'production'
+    
+    qa {
+    	ignore = true
     }
-    dev{
-        stage = 'dev'
+    
+    sandbox {
+    	ignore = true
+    }
+    
+    prod {
+    	ignore = true
     }
 }
+
+release_active = true
